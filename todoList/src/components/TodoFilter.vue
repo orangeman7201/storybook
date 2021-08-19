87e3516fcc66b850bc7f11d11a846d25c2953e3f
unknown <aki.jazz1027@gmail.com>
2021-08-20 01:37:04 +0900

    create VueComponent's storied.js

--- todoList/src/components/TodoFilter.vue
+++ todoList/src/components/TodoFilter.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <input type="radio" id="all" value="all" v-model="radioState">全て
-    <input type="radio" id="working" value="working" v-model="radioState">作業中
-    <input type="radio" id="finish" value="finish" v-model="radioState">完了
-  </div>
+  <form>
+    <input type="radio" id="all" value="all" v-model="radioState"><rabel for="all">全て</rabel>
+    <input type="radio" id="working" value="working" v-model="radioState"><rabel for="working">作業中</rabel>
+    <input type="radio" id="finish" value="finish" v-model="radioState"><rabel for="finish">完了</rabel>
+  </form>
 </template>
 
 <script>
